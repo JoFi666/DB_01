0ff5be13a7baeaad9eedd62d79a9a25b95889af6
JoFi <Jonas@hildebrandts.info>
2023-11-29 12:27:03 +0100

    1

---
+++ Aufgabenblatt3/testTuppel.sql
@@ -0,0 +1,13 @@
+INSERT INTO Patienten(PatientenID, Vorname, Nachname, Adresse, Geburtsdatum, Versichertenstatus)
+VALUES (1, Franz, Gilbert, "Elsterweg 7" , 05.12.2003, privat),
+       (2, Herbert, Mayer, "Berliner Straße 18", 12.05.1988, gesetzlich),
+       (3, Gisela, Petersen, "Franziskanerweg 5", 01.01.1960, gesetzlich),
+       (4, Magdalena, Walther, "Querstraße 60", 25.10.1999, null),
+       (5, Frauke, Eberth, "Bahnhofstraße 5", 13.12.1975, privat),
+       (6, Peter, Mensch, "Hirschhalde 45", 30.09.1997, gesetzlich),
+       (7, Leon, Quack, "Mardersteige 12", 02.07.2008, gesetzlich),
+       (8, Annalena, Sturm, "Querstraße 61", 09.09.2009, privat),
+       (9, Leah, Müller, "Birnenallee 201", 24.04.1947, gesetzlich),
+       (10, Gerda, Krug, "Mandelweg 4", 07.09.1969, privat);
+
+INSERT INTO Apotheke(ApothekenID, Name, Adresse)
